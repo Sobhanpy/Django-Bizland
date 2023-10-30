7ca053c651908f852ddafe84bc0204ac5e4cd919
Sobhan Moradi <sobhan.mrds@gmail.com>
2023-10-30 17:18:30 +0330

    last

--- root/admin.py
+++ root/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import *
 
+admin.site.register(Skiils)
 # Register your models here.
--- root/models.py
+++ root/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Skiils(models.Model):
+    title = models.CharField(max_length=150)
+    persent = models.IntegerField(max_length=3)
